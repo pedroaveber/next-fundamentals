be16545373ff73e356792eac60e9dc5c50e26c2c
pedroaveber <pedro.veber@outlook.com>
2024-04-11 09:39:01 -0300

    fix: create suspense boundary on search-form

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,15 +1,13 @@
 import { Header } from '@/components/header'
 import { CartContextProvider } from '@/contexts/cart-context'
-import { ReactNode, Suspense } from 'react'
+import { ReactNode } from 'react'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
     <CartContextProvider>
       <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8">
-        <Suspense>
-          <Header />
-          {children}
-        </Suspense>
+        <Header />
+        {children}
       </div>
     </CartContextProvider>
   )
